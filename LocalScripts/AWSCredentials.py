9742dfb275411546f7a238dba6e4761d0201b204
lmf90409 <mol017@ucsd.edu>
2014-05-19 21:21:53 -0700

    no message

--- LocalScripts/AWSCredentials.py
+++ LocalScripts/AWSCredentials.py
@@ -1,7 +1,7 @@
 ### AWS credentials: ####
 aws_access_key_id='AKIAJ7TG5BXBG255MTYA'
 aws_secret_access_key='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'
-keyPairFile="/Users/apple/UCSD_BigData/LocalScripts/tryandtry.pem" # name of file keeping local key
+keyPairFile="/Users/apple/UCSD_BigData/Vault/tryandtry.pem" # name of file keeping local key
 key_name="tryandtry" # name of keypair (not name of file where key is stored)
 # Set the security group On the EC2 page (You will need to add IP addresses if
 # you want to connect from a place previously unauthorized.
